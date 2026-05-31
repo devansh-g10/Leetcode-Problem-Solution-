4e70c9f20786683b7531265c881d9dd4551f59ac
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-06-01 02:23:11 +0530

    Time: 1 ms (100%), Space: 42.6 MB (100%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,16 +1,12 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        int[] freq = new int[10];
+        int res = 0;
+
         while (n > 0) {
-            int digit = n % 10;
-            freq[digit]++;
+            res += n % 10;
             n /= 10;
         }
-        int score = 0;
-        for (int d = 0; d <= 9; d++) {
-            score += d * freq[d];
-        }
 
-        return score;
+        return res;
     }
 }
