26f263ea09c69d4e19f29a658161cd271eefa11a
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-06-26 15:01:21 +0530

    Time: 2 ms (100%), Space: 87.8 MB (81.97%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -1,59 +1,24 @@
-import java.util.*;
-
 class Solution {
     public long countMajoritySubarrays(int[] nums, int target) {
         int n = nums.length;
-
-        long[] prefix = new long[n + 1];
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + (nums[i] == target ? 1 : -1);
-        }
-
-        long[] sorted = prefix.clone();
-        Arrays.sort(sorted);
-
-        Map<Long, Integer> map = new HashMap<>();
-        int idx = 1;
-        for (long x : sorted) {
-            if (!map.containsKey(x)) {
-                map.put(x, idx++);
-            }
-        }
-
-        Fenwick bit = new Fenwick(idx + 2);
-
+        // represents the occurrence count of prefix sums -n, -(n-1), ..., 0, 1, ..., n, with index offset by n.
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int cnt = n;
         long ans = 0;
-
-        for (long x : prefix) {
-            int id = map.get(x);
-            ans += bit.query(id - 1);
-            bit.update(id, 1);
+        long presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            } else {
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
         }
-
         return ans;
-    }
-
-    static class Fenwick {
-        int[] tree;
-
-        Fenwick(int n) {
-            tree = new int[n + 2];
-        }
-
-        void update(int i, int delta) {
-            while (i < tree.length) {
-                tree[i] += delta;
-                i += i & -i;
-            }
-        }
-
-        int query(int i) {
-            int sum = 0;
-            while (i > 0) {
-                sum += tree[i];
-                i -= i & -i;
-            }
-            return sum;
-        }
     }
 }
