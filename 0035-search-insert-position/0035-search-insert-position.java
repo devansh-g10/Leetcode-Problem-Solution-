2847637a3f26b35f3478f7293dcd0fe716704387
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-08-15 13:30:03 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.83%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,20 +2,18 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int s = 0;
         int e = nums.length - 1;
-        int ans = nums.length;
         while(s<=e){
             int mid = s + (e-s)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid] > target){
-                ans = mid;
+            // if(nums[mid]==target){
+            //     return mid;
+            // }
+            if(nums[mid] >= target){
                 e = mid - 1;
             }
             else{
                 s = mid + 1;
             }
         }
-        return ans;
+        return s;
     }
 }
