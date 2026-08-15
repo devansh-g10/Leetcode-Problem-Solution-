95bb5ca0bdcad8f78c91c55848bfed51e4189b57
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-08-15 14:05:32 +0530

    Time: 0 ms (100%), Space: 48 MB (88.22%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,50 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int s = firstPosition(nums,target);
-        int e = LastPosition(nums,target);
-        return new int[] {s,e};
+        int s = firstPosition(nums, target);
+        int e = lastPosition(nums, target);
+
+        return new int[] {s, e};
     }
 
-    public int firstPosition(int[] nums , int target){
-        int left = 0;
-        int right = nums.length - 1;
-        int floor = -1;
-        while(left<= right){
-            int mid = left + (right - left)/2;
+    public int firstPosition(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        int ans = -1;
 
-            if(nums[mid]==target){
-                floor = mid;
-                right = mid -1;
-            }
-            else if (nums[mid] > target){
-                right = mid - 1;
-            }
-            else{
-                left = mid +1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] >= target) {
+                if (nums[mid] == target) {
+                    ans = mid;
+                }
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
-        return floor;
+
+        return ans;
     }
-    static int LastPosition(int[] nums , int target){
-        int left = 0;
-        int right = nums.length-1;
-        int ceil = -1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target){
-                ceil = mid;
-                left = mid +1;
-            }
-            else if(nums[mid]>target){
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
+
+    public int lastPosition(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        int ans = -1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] <= target) {
+                if (nums[mid] == target) {
+                    ans = mid;
+                }
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
         }
-        return ceil;
+
+        return ans;
     }
 }
