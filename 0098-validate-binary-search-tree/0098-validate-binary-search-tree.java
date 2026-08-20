24dde55c725cdf13f5a104dcf3bfe252fb69d0dd
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-08-20 12:14:17 +0530

    Time: 2 ms (12.8%), Space: 45.5 MB (10.71%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,10 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);        
+    static void inorder(TreeNode root , List<Integer> list){
+        if(root == null){
+            return;
+        }
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+
     }
-    private boolean valid(TreeNode node, long minimum, long maximum) {
-        if (node == null) return true;
-        if (!(node.val > minimum && node.val < maximum)) return false;
-        return valid(node.left, minimum, node.val) && valid(node.right, node.val, maximum);
-    }    
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root,list);
+        for(int i = 0 ;i<list.size()-1;i++){
+            if(list.get(i) >= list.get(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
