9120197955e4b4869f76b3b5147e594ac306743a
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-09-11 01:27:47 +0530

    Time: 7 ms (84.76%), Space: 60.8 MB (40.41%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -2,6 +2,7 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1) {
@@ -9,18 +10,23 @@ class Solution {
                 }
             }
         }
-        return Arrays.stream(grid).mapToInt(row -> Arrays.stream(row).sum()).sum();
-    }
-    
-    private void dfs(int[][] grid, int i, int j) {
-        grid[i][j] = 0;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int[] direction : directions) {
-            int x = i + direction[0];
-            int y = j + direction[1];
-            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
-                dfs(grid, x, y);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
             }
         }
+        return count;
+    }
+    private void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length ||j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i, j - 1); // left
+        dfs(grid, i, j + 1); // right
     }
 }
