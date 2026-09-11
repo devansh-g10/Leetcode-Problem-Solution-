35f2994897e290330fd8495ec545f7fb26f6d242
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-09-11 15:38:27 +0530

    Time: 0 ms (100%), Space: 42 MB (54.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
         if(n<=2) return n;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp , -1);
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3 ; i <= n ; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        int x = 1;
+        int y = 2;
+        for(int i = 3 ; i<=n ; i++){
+            int temp = x+y;
+            x = y;
+            y = temp;
         }
-        return dp[n];
+        return y;
     }
 }
