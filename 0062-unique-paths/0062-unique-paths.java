90b32ed01f837560479821927fe6fc45ee742a41
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-09-18 23:07:36 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // 2D-DP Bottom-Up Approach(Iterative Version)
-        int[][] dp = new int[m][n];
-        for(int[] num : dp){
-            Arrays.fill(num,-1);
+        int totalN = m+n-2;
+        int totalR = m-1;
+        long res = 1;
+        for(int i=1; i <= totalR; i++){
+            res = res * (totalN - totalR + i) / i;
         }
-        dp[m-1][n-1] = 1;
-        for(int i = m-1 ; i>=0 ; i--){
-            for(int j = n-1 ; j>=0 ; j--){
-                if(i == m-1 && j==n-1) continue;
-                int down = 0;
-                int right = 0;
-                if(j+1 != n) right = dp[i][j+1];
-                if(i+1 != m) down = dp[i+1][j];
-                dp[i][j] = right + down;
-            }
-        }
-        return dp[0][0];
+        return (int) res;
+        
     }
 }
