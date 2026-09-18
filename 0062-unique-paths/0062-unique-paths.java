1373a56b20512564e59c1f576255dffc223f9664
DEVANSH MAHESHWARI <devanshmaheshwari3011@gmail.com>
2026-09-18 11:42:37 +0530

    Time: 1 ms (13.39%), Space: 42 MB (81.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // 2D-DP Top-Down Approach(Recursivee)
-        int i = 0;
-        int j = 0;
-        int[][] dp = new int[m+1][n+1];
+        // 2D-DP Bottom-Up Approach(Iterative Version)
+        int[][] dp = new int[m][n];
         for(int[] num : dp){
             Arrays.fill(num,-1);
         }
-        return solve(m,n,0,0,dp);
-    }
-    public int solve(int m , int n , int i , int j , int[][] dp){
-        if(i == m-1 && j == n-1){
-            return 1;
+        dp[m-1][n-1] = 1;
+        for(int i = m-1 ; i>=0 ; i--){
+            for(int j = n-1 ; j>=0 ; j--){
+                if(i == m-1 && j==n-1) continue;
+                int down = 0;
+                int right = 0;
+                if(j+1 != n) right = dp[i][j+1];
+                if(i+1 != m) down = dp[i+1][j];
+                dp[i][j] = right + down;
+            }
         }
-        if(i==m || j == n){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        } 
-        dp[i][j] = solve(m,n,i,j+1,dp) + solve(m,n,i+1,j,dp);
-        return dp[i][j];
+        return dp[0][0];
     }
 }
